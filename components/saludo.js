718cc4bb60ceb125431fb0dbabd116d5f8e1fcae
Sebastian Roldan <sroldan@herfox.com>
2020-08-30 22:38:42 -0500

    Componentes Basicos

---
+++ components/saludo.js
@@ -0,0 +1,14 @@
+Vue.component('saludo',{
+    template: //
+    `
+    <div>
+        <h1>{{ saludo }}</h1>
+        <h3>{{ sdbgfhgfd }}</h3>
+    </div>
+    `,
+    data(){
+        return {
+            saludo: 'Hola desde Vue, con Componente'
+        }
+    }
+})
