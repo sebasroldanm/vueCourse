7cdeaaf587f0c9da2c850aa72bef8a250191a775
Sebastian Roldan <sroldan@herfox.com>
2020-08-30 19:09:15 -0500

    Acceder a Model

--- 01.js
+++ 01.js
@@ -2,10 +2,26 @@ const app = new Vue({
     el: '#app',
     data: {
         titulo: 'Hola mundo con Vue',
-        frutas: [
-            {nombre: 'Pera', cantidad: 10},
-            {nombre: 'Manzana', cantidad: 0},
-            {nombre: 'Platano', cantidad: 12}
-        ]
+        frutas: [{
+                nombre: 'Pera',
+                cantidad: 10
+            },
+            {
+                nombre: 'Manzana',
+                cantidad: 0
+            },
+            {
+                nombre: 'Platano',
+                cantidad: 12
+            }
+        ],
+        nuevaFruta: ''
+    },
+    methods: {
+        agregarFruta() {
+            this.frutas.push({
+                nombre: this.nuevaFruta, cantidad: 0
+            })
+        }
     }
 });
