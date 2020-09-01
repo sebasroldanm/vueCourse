d1bd5a37b273ea26840e65af200dd1c139999503
Sebastian Roldan <sroldan@herfox.com>
2020-09-01 15:33:12 -0500

    Mostrar Datos
    Por medio de v-for se muestra, usando dos componentes, ademas, se entrega un listado ordenado por cantidad (función Js)

---
+++ proyecto-2/src/components/Lista.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul class="list-group">
+      <!-- <li
+        v-for="(fruta, index) of frutas" :key="fruta.id" @click="aumentar(index)"
+        class="list-group-item d-flex justify-content-between align-items-center"
+      > -->
+      <li
+        v-for="(fruta, index) of arrayOrdenado" :key="fruta.id" @click="aumentar(index)"
+        class="list-group-item d-flex justify-content-between align-items-center"
+      >
+        {{ index }}. {{ fruta.nombre }}
+        <span class="badge badge-primary badge-pill">{{ fruta.cantidad }}</span>
+      </li>
+    </ul>
+    <button class="btn btn-danger btn-block" @click="reiniciar">Reiniciar</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  name: "Lista",
+  computed: {
+    ...mapState(["frutas"]),
+    arrayOrdenado() {
+        return this.frutas.sort((a,b) => b.cantidad - a.cantidad )
+    }
+  },
+  methods: {
+      ...mapMutations(['aumentar', 'reiniciar'])
+  },
+};
+</script>
--- proyecto-2/src/store/index.js
+++ proyecto-2/src/store/index.js
@@ -5,8 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    frutas: [
+      { nombre: 'Manzana', cantidad: 11 },
+      { nombre: 'Fresa', cantidad: 10 },
+      { nombre: 'Naranja', cantidad: 4 }
+    ]
   },
   mutations: {
+    aumentar(state, index) {
+      state.frutas[index].cantidad ++
+    },
+    reiniciar(state) {
+      state.frutas.forEach(fruta => {
+        fruta.cantidad = 0
+      });
+    }
   },
   actions: {
   },
