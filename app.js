7aa7d31453931f1835a5c00803e82b62fe0a1b45
Sebastian Roldan <sroldan@herfox.com>
2020-10-12 17:58:08 -0500

    Dos App

--- app.js
+++ app.js
@@ -16,4 +16,14 @@ const app = Vue.createApp({
   });
   
   app.mount('#app');
+
+
+  const app2 = Vue.createApp({
+    data() {
+      return {
+        comidaFav: 'Pizza'
+      }
+    }
+  });
   
+  app2.mount('#app2')
