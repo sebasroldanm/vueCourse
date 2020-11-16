4414a191a7039c88f95292b025272e7a072e3af4
Sebastian Roldan <sroldan@herfox.com>
2020-11-16 10:47:40 -0500

    Solución Show and Hide details
    Solución al problema usando un componente para este caso

---
+++ src/Components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toogleDetails">Show details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Emial: </strong>{{ friend.emial }}</li>
+    </ul>
+  </li>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lopez",
+        phone: "+57 312 6529966",
+        email: "manuel@app.com",
+      },
+    };
+  },
+  methods: {
+      toogleDetails() {
+          this.detailsAreVisible = !this.detailsAreVisible;
+      }
+  },
+};
+</script>
