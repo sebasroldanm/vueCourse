61ec90069ff281295362c25fa458a7c834b0e396
Sebastian Roldan <sroldan@herfox.com>
2020-11-16 10:34:10 -0500

    Uso de templates en CLI

---
+++ vue-project/src/App.vue
@@ -0,0 +1,32 @@
+<template lang="">
+    <section>
+        <h2>My friends</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lopez',
+                    phone: '+57 312 6529966',
+                    email: 'manuel@app.com'
+                },
+                {
+                    id: 'lina',
+                    name: 'Lina Contreras',
+                    phone: '+57 300 2546632',
+                    email: 'lina@app.com'
+                },
+            ]
+        }
+    },
+}
+</script>
