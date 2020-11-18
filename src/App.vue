53319dc6c56248625354432cda0edca05797f31c
Sebastian Roldan <sroldan@herfox.com>
2020-11-18 16:41:51 -0500

    New Compo. y emitiendo de hijo a padre

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
         <header>
             <h1>My friends</h1>
         </header>
+        <new-friend @add-contact="addContact"></new-friend>
         <ul>
             <friend-contact
               v-for="friend in friends"
@@ -20,6 +21,7 @@
 
 
 <script>
+// import FriendContact from "./Components/FriendContact.vue";
 export default {
   data() {
     return {
@@ -47,6 +49,17 @@ export default {
         (friend) => friend.id === friendId
       );
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false,
+      };
+      this.friends.push(newFriendContact);
     },
   },
 };
@@ -85,7 +98,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -117,5 +131,19 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ src/Components/NewFriend.vue
@@ -0,0 +1,37 @@
+<template>
+    <form @submit.prevent="submitData">
+        <div>
+            <label>Name</label>
+            <input type="text" v-model="enteredName">
+        </div>
+        <div>
+            <label>phone</label>
+            <input type="tel" v-model="enteredPhone">
+        </div>
+        <div>
+            <label>E-Mail</label>
+            <input type="mail" v-model="enteredEmail">
+        </div>
+        <div>
+            <button>Add Contact</button>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    emits: ['add-contact'],
+    data() {
+        return {
+            enteredName: '',
+            enteredPhone: '',
+            enteredEmail: '',
+        }
+    },
+    methods: {
+        submitData() {
+            this.$emit('add-contact', this.enteredName, this.enteredPhone, this.enteredEmail)
+        }
+    },
+}
+</script>
